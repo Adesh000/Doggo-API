8748a97c44624bb2d7e030aaee27fd415deb4414
Adesh Nayak <adeshnayak6@gmail.com>
2022-12-04 21:26:47 +0530

    created breed list

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,29 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./home.css";
 
 const Home = () => {
-    const navigate = useNavigate()
-    const userName = JSON.parse(localStorage.getItem('user'))
+    const navigate = useNavigate();
+    const userName = JSON.parse(localStorage.getItem("user"));
 
     const handleLogout = () => {
-        localStorage.removeItem('loggedIn')
+        localStorage.removeItem("loggedIn");
         navigate("/login");
-    }
+    };
 
     const [dogInfo, setDogInfo] = useState([]);
+    const [dogBreed, setDogbreed] = useState('')
 
     const allBreeds = () => {
-        fetch('https://dog.ceo/api/breeds/list/all').then(res => res.json()).then(data => {
-            return setDogInfo(data.message)
-        })
-    }
+        fetch("https://dog.ceo/api/breeds/list/all")
+            .then((res) => res.json())
+            .then((data) => {
+                let breedNames = Object.keys(data.message);
+                return setDogInfo(breedNames);
+            });
+        console.log(dogInfo);
+    };
+
+    console.log(dogBreed);
     return (
         <div>
-            <button onClick={allBreeds}>All Breeds</button>
-            <div>{dogInfo[5]}</div>
-            <button onClick={handleLogout}>
-                Logout
-            </button>
+            <div className="btn">
+                <button onClick={allBreeds}>All Breeds</button>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+            <div>
+                <div>
+                    {dogInfo.map((element, index) => {
+                        return (
+                            <div key={index}>
+                                <ul>
+                                    <li> <button onClick={e => setDogbreed(e)}>{element}</button></li>
+                                </ul>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div></div>
+            </div>
         </div>
     );
 };
